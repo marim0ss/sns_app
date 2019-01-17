b045c175e7259dd280eaedc90d0fb9310c22184d
marim0ss <piyomaru.bunbun@gmail.com>
2019-01-17 19:24:20 +0900

    ログインユーザーはcurrent_user変数を使用

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def create
     Post.create(
       content: posts_params[:content],
-      user_id: session[:user_id]
+      user_id: current_user.id
     )
     redirect_to posts_path
   end
