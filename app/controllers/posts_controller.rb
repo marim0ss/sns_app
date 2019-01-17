90f75f5169f79ca94db02d2cde5700addc63b936
marim0ss <piyomaru.bunbun@gmail.com>
2019-01-17 18:11:47 +0900

    投稿の編集・更新・削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(posts_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+
   private
     def posts_params
       params.require(:post).permit(:content)
