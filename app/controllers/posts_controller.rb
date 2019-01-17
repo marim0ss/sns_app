e225985c4a0e416f0a207333041936811c2650c8
marim0ss <piyomaru.bunbun@gmail.com>
2019-01-17 16:21:03 +0900

    投稿一覧、新規投稿、詳細ページ追加、アソシエーション記述、時刻を日本表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
+
+  def new
+  @post = Post.new
+  end
+
+  def create
+    Post.create(posts_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+
+
+  has_many :posts
 end
